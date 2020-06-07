eed5e2b9bbe6a492adb6fb08b1de4f93ebd2cdc4
JarosLucky <snyashka@gmail.com>
2020-06-08 01:18:30 +0700

    cleanup

--- blues-atm/lua/batm_scenes/loading.lua
+++ blues-atm/lua/batm_scenes/loading.lua
@@ -11,14 +11,6 @@ surface.CreateFont( "batm_loading_text", {
 	blursize = 0,
 	scanlines = 0,
 	antialias = true,
-	underline = false,
-	italic = false,
-	strikeout = false,
-	symbol = false,
-	rotary = false,
-	shadow = false,
-	additive = false,
-	outline = false,
 } )
 
 local scene = {}
