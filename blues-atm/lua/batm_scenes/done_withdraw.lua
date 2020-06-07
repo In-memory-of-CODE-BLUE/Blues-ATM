64a7521ea2c993d6f490a878959bff72e7b7bc6a
JarosLucky <snyashka@gmail.com>
2020-06-08 01:16:51 +0700

    cleanup

--- blues-atm/lua/batm_scenes/done_withdraw.lua
+++ blues-atm/lua/batm_scenes/done_withdraw.lua
@@ -12,14 +12,6 @@ surface.CreateFont( "batm_done_text", {
 	blursize = 0,
 	scanlines = 0,
 	antialias = true,
-	underline = false,
-	italic = false,
-	strikeout = false,
-	symbol = false,
-	rotary = false,
-	shadow = false,
-	additive = false,
-	outline = false,
 } )
 
 local scene = {}
