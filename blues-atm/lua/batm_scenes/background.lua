56acabde5a8e130cc3ceb687919a459778a6f9d4
JarosLucky <snyashka@gmail.com>
2020-06-08 01:15:33 +0700

    cleanup

--- blues-atm/lua/batm_scenes/background.lua
+++ blues-atm/lua/batm_scenes/background.lua
@@ -10,14 +10,6 @@ surface.CreateFont( "batm_date", {
 	blursize = 0,
 	scanlines = 0,
 	antialias = true,
-	underline = false,
-	italic = false,
-	strikeout = false,
-	symbol = false,
-	rotary = false,
-	shadow = false,
-	additive = false,
-	outline = false,
 } )
 
 surface.CreateFont( "batm_time", {
@@ -28,14 +20,6 @@ surface.CreateFont( "batm_time", {
 	blursize = 0,
 	scanlines = 0,
 	antialias = true,
-	underline = false,
-	italic = false,
-	strikeout = false,
-	symbol = false,
-	rotary = false,
-	shadow = false,
-	additive = false,
-	outline = false,
 } )
 
 surface.CreateFont( "batm_large", {
@@ -46,14 +30,6 @@ surface.CreateFont( "batm_large", {
 	blursize = 0,
 	scanlines = 0,
 	antialias = true,
-	underline = false,
-	italic = false,
-	strikeout = false,
-	symbol = false,
-	rotary = false,
-	shadow = false,
-	additive = false,
-	outline = false,
 } ) 
 
 local logo = Material("bluesatm/logo.png", "noclamp smooth")
